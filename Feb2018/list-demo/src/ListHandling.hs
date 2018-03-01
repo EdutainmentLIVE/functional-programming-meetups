ec64ef05ca251f73064b1332e4acd8bc079d0985
Cameron Gera <gera.cameron@gmail.com>
2018-03-01 08:26:06 -0500

    Made sure hello-haskell compiles

--- Feb2018/list-demo/src/ListHandling.hs
+++ Feb2018/list-demo/src/ListHandling.hs
@@ -6,12 +6,15 @@ import Data.List (foldl')
 
 listPrinter :: IO ()
 listPrinter = do
-  print sampleList
   print $ map addTen sampleList
+  print sampleList
   print $ foldl' (+) 0 sampleList
 
 addTen :: Int -> Int
 addTen x = x + 10
 
 sampleList :: [Int]
-sampleList = [ 1, 3, 5, 6, 23, 25, 11, 10, 7, 22, 31]
+sampleList = [1, 3, 5, 6, 23, 25, 11, 10, 7, 22, 31]
+
+-- 1 [ 3, ...]
+-- 4 []
